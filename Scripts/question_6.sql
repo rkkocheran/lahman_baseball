80edd945c5010e22e0e6e99eec01a0dd5c3e0fe2
Rachel Kocheran <rkkocheran@gmail.com>
2022-10-04 15:38:14 -0500

    started question 6

---
+++ Scripts/question_6.sql
@@ -0,0 +1,10 @@
+/* Q6
+   Find the player who had the most success stealing bases in 2016, 
+   where __success__ is measured as the percentage of stolen base attempts which are successful. 
+   (A stolen base attempt results either in a stolen base or being caught stealing.) 
+   Consider only players who attempted _at least_ 20 stolen bases.
+   
+   A:  */
+
+SELECT * 
+FROM 
