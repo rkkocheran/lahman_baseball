85cc371d60373e403a7ae78074546367df01d17b
Adam Crank <adam.crank@outlook.com>
2022-10-05 13:19:45 -0500

    pushing 9 for review

--- scripts/question_5.sql
+++ scripts/question_5.sql
@@ -3,8 +3,8 @@ you report to 2 decimal places. Do the same for home runs per game. Do you see a
 --Strikeouts (so), Homeruns (hr) in pitching table
 
 SELECT CONCAT(LEFT(yearid::text,3),'0''s') AS decade, 
-	ROUND(AVG(so),2) AS avg_so,
-	ROUND(AVG(hr),2) AS avg_hr
+	ROUND(AVG(so/g),2) AS avg_so_per_game,
+	ROUND(AVG(hr/g),2) AS avg_hr_per_game
 FROM pitching
 WHERE LEFT(yearid::text,3)::integer >= 192
 GROUP BY LEFT(yearid::text,3)
