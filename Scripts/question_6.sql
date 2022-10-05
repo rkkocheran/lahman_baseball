8102af60ce599155f4e2a08412ce84a7a801646d
rkkocheran <rkkocheran@gmail.com>
2022-10-05 10:03:29 -0500

    began question 6

--- Scripts/question_6.sql
+++ Scripts/question_6.sql
@@ -4,7 +4,20 @@
    (A stolen base attempt results either in a stolen base or being caught stealing.) 
    Consider only players who attempted _at least_ 20 stolen bases.
    
-   A:  */
+   A: Chris Owings with 21 successfully stolen bases and 23 total attempts (91.3% success rate). */
 
-SELECT * 
-FROM 
+SELECT namefirst,
+       namelast,
+       ROUND((SUM(sb)::numeric / (SUM(sb) + SUM(cs)) * 100.0), 1) AS percent_success,
+       (SUM(sb) + SUM(cs)) AS n_attempts,
+       SUM(sb) AS n_success,
+       SUM(cs) AS n_fail
+FROM batting AS b
+INNER JOIN people USING (playerid)
+WHERE b.yearid = 2016
+GROUP BY playerid,
+         namefirst,
+         namelast
+HAVING (SUM(sb) + SUM(cs)) >= 20
+ORDER BY percent_success DESC
+LIMIT 1;
