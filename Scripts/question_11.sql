1050934eafd246372ed12842e86377fe1cb3f856
rkkocheran <rkkocheran@gmail.com>
2022-10-05 13:48:17 -0500

    finished question 11

--- Scripts/question_11.sql
+++ Scripts/question_11.sql
@@ -3,16 +3,18 @@
    As you do this analysis, keep in mind that salaries across the whole league tend to increase together, so you may want 
    to look on a year-by-year basis. */
 
-SELECT teamid,
-       g,
-       w,
-       l,
-       t.lgid,
-       s.playerid,
-       s.yearid,
-       salary,
-       (AVG(salary) OVER (PARTITION BY teamid, s.yearid))::NUMERIC::MONEY AS avg_salary
+/* Used a calculated field to determine the average salary for each team and year. With an addition calculated field, 
+   dol_per_win_thous, to determine the dollars per win in 'thousands' for each team and year, showing how salary has 
+   increased over time. Using pivot tables/charts to analyze the data indicates there does appear to be a loose 
+   correlation between wins and salary. */
+   
+SELECT DISTINCT teamid,
+                s.yearid,
+                w,
+                (AVG(salary) OVER (PARTITION BY teamid, s.yearid))::NUMERIC::MONEY AS avg_salary,
+                (((AVG(salary) OVER (PARTITION BY teamid, s.yearid))::NUMERIC / w) / 1000.0)::MONEY AS dol_per_win_thous
 FROM teams AS t
 INNER JOIN salaries AS s USING (teamid)
 WHERE t.yearid >= 2000
       AND t.yearid = s.yearid
+ORDER BY teamid, s.yearid
