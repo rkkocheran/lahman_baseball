b22d215a6b0e759609a97c54773a4b9f43b2d7b9
Morgan-Hines <mkeiser19@gmail.com>
2022-10-05 14:42:32 -0400

    final push question 10

--- Scripts/question_10.sql
+++ Scripts/question_10.sql
@@ -1,14 +1,15 @@
 --QUESTION 10
 
-WITH years_played AS (SELECT *, ((finalgame::date-debut::date)/365) as years_playing
-					  FROM people),
-WITH tenyr_players AS (SELECT *
-						FROM batting
-						INNER JOIN years_played USING (playerid)
-						WHERE years_playing >= 10
-						AND yearid = 2016
-						AND hr >= 1)
-SELECT *
-FROM
-
+WITH players_w_2016_hr AS (SELECT *, ((finalgame::date-debut::date)/365) as years_playing, MAX(batting.hr) OVER(PARTITION BY playerid) as career_high
+					  FROM people
+					  INNER JOIN batting USING (playerid)
+					  WHERE yearid = 2016
+					  AND hr >= 1)
+SELECT DISTINCT playerid, namefirst, namelast, batting.hr as homeruns_2016
+FROM batting
+INNER JOIN players_w_2016_hr USING (playerid)
+WHERE years_playing >= 10
+AND batting.hr >= career_high
+AND batting.yearid = 2016
+ORDER BY homeruns_2016 ASC;
 
