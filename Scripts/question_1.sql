f9f7c46da1586a78f10a4012b5b4ffb6264189cf
Rachel Kocheran <112971094+rkkocheran@users.noreply.github.com>
2022-11-14 17:01:17 -0600

    Delete Scripts directory

--- Scripts/question_1.sql
+++
@@ -1,3 +0,0 @@
---1. What range of years for baseball games played does the provided database cover?
-SELECT DISTINCT MIN(yearid) AS first_year, MAX(yearid) AS last_year
-FROM appearances;
--- Scripts/question_4.sql
+++
@@ -1,8 +0,0 @@
---QUESTION 4
-SELECT SUM(po) AS putouts,
-  CASE WHEN pos = 'OF' THEN 'Outfield'
-	 WHEN pos ILIKE '%B' OR pos ILIKE 'SS' THEN 'Infield'
- 	 ELSE 'Battery' END AS position
-FROM fielding
-WHERE yearid = 2016
-GROUP BY position;
